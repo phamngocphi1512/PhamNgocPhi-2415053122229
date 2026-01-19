810f540edb83ef4ff53c5510d5ea8987ba1246f3
phamngocphi1512 <phamngocphi1512@gmail.com>
2026-01-19 14:30:13 +0700

    Phi

---
+++ PhamNgocPhi_2415053122229/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Họ và tên: Phạm Ngọc Phi");
+        Console.WriteLine("Mã sinh viên: 2415053122229");
+
+        Console.ReadLine();
+    }
+}
