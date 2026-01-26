a1d88e0083997193fe31edd9ff4dd9d21d6b9c03
Phạm Ngọc Phi <phamngocphi1512@gmail.com>
2026-01-26 14:35:24 +0700

    Bài Tập Chuỗi

---
+++ Bai2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Bai2
+{
+    public static void Run()
+    {
+        Console.Write("Nhap chuoi: ");
+        string s = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            Console.WriteLine("So tu: 0");
+            return;
+        }
+
+        s = s.Trim();
+
+        string[] arr = s.Split(
+            new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        Console.WriteLine("So tu: " + arr.Length);
+    }
+}
